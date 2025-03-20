27e8e357fc963f53243f57b42c8a6b1231e76d0d
MARIA WANYAMA <daphinewanyama@gmail.com>
2025-03-20 20:08:16 +0300

    hey

---
+++ TINDO_MEDIA/Home/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path,include
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('Home/', include('Home.urls'))
+# ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about_us, name='about_us'),
+    path('blog/', views.blog, name='blog'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact_us, name='contact_us'),
+]
--- TINDO_MEDIA/Home/views.py
+++ TINDO_MEDIA/Home/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
+
+
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+def contact_us(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_us')  # Redirect to the same page after submission
+    else:
+        form = ContactForm()
+    return render(request, 'contact_us.html', {'form': form})
