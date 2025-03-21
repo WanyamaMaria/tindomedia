759cb29e684447b127c9eec1b8918ebf05f36954
MARIA WANYAMA <daphinewanyama@gmail.com>
2025-03-21 17:09:43 +0300

    heyy

--- TINDO_MEDIA/TINDO_MEDIA/settings.py
+++ TINDO_MEDIA/TINDO_MEDIA/settings.py
@@ -123,10 +123,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email settings
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'mail.djangolearn.xyz'  # Replace with your SMTP server
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'daphinewanyama@gmail.com'
-# EMAIL_HOST_PASSWORD = '123ranzo'
+
+
